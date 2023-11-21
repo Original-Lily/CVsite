d1cc62ec9c4337aacb43ffe522435fb3db7665dc
Lily <87139613+Original-Lily@users.noreply.github.com>
2023-11-21 19:43:39 +0000

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,10 +7,10 @@ const data = [
     image: '/images/projects/CloakRoom.png',
     date: '2023-01-12',
     desc:
-      'An easy to use and professional website in order to demonstrate my skills & attributes, ' +
-      'tasks still todo being: ' +
-      'include a German/English toggle, ' +
-      'and to create a dark/light mode toggle',
+      'An easy to use and professional website in order to demonstrate my skills & attributes,',
+      + 'tasks still todo being:,',
+      + 'include a german/ english toggle,',
+      + '& to create a dark/ light mode toggle'
   },
   {
     title: 'Lilys Diskwatch',
@@ -19,19 +19,19 @@ const data = [
     image: '/images/projects/DiskWatch.png',
     date: '2021-06-12',
     desc:
-      'Lightweight Disk utilities ' +
-      'Calculates the SHA512 hash for each file in the "Files" directory. Creates or overwrites a baseline file ("baseline.txt") with file paths and corresponding hash values.',
+      'Lightweight Disk utilities',
+      + 'Calculates the SHA512 hash for each file in the "Files" directory. Creates or overwrites a baseline file ("baseline.txt") with file paths and corresponding hash values.'
   },
   {
-    title: 'Unnamed Game',
-    subtitle: 'Created at 14, don\'t judge me',
+    title: 'Unamed Game',
+    subtitle: 'Created at 14, dont judge me',
     link: 'https://github.com/Original-Lily/CuteGame',
     image: '/images/projects/NonameGame.png',
     date: '2015-10-11',
     desc:
-      'A simple yet addictive python game, developed at first for fun, ' +
-      'then adapted into a much smaller version in order to evade ' +
-      'my school\'s internal security systems',
+      'A simple yet addictive python game, developed at first for fun,'
+      + 'then adapted into a much smaller version in order to evade',
+      + 'my schools internal security systems'
   },
 ];
 
