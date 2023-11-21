efb9db55cbad57d9d3b2e35125f37fd77f7793ad
Lily <87139613+Original-Lily@users.noreply.github.com>
2023-11-21 17:09:13 +0000

    Update personal.js

--- src/data/stats/personal.js
+++ src/data/stats/personal.js
@@ -28,8 +28,6 @@ const data = [
     key: 'countries',
     label: 'Countries visited',
     value: 4,
-    /*link:
-      'https://www.google.com/maps/d/embed?mid=1iBBTscqateQ93pWFVfHCUZXoDu8&z=2',*/
   },
   {
     key: 'location',
