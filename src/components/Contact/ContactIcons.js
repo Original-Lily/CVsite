49ca6cc5f29b7eb0600df69dca25a1e80e02a1df
Lily <87139613+Original-Lily@users.noreply.github.com>
2023-11-22 06:47:47 +0000

    Update ContactIcons.js

--- src/components/Contact/ContactIcons.js
+++ src/components/Contact/ContactIcons.js
@@ -1,19 +1,29 @@
+// ContactIcons.js
 import React from 'react';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-import data from '../../data/contact';
+import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
 
 const ContactIcons = () => (
-  <ul className="icons">
-    {data.map((s) => (
-      <li key={s.label}>
-        <a href={s.link}>
-          <FontAwesomeIcon icon={s.icon} />
-        </a>
-      </li>
-    ))}
-  </ul>
+  <div className="icons">
+    <a
+      href="https://www.linkedin.com/in/lily-bradshaw-03a28b1b7/"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="LinkedIn"
+    >
+      <FaLinkedin />
+    </a>
+    <a
+      href="https://github.com/Original-Lily"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="GitHub"
+    >
+      <FaGithub />
+    </a>
+    <a href="mailto:buddie9173@gmail.com" aria-label="Email">
+      <FaEnvelope />
+    </a>
+  </div>
 );
 
 export default ContactIcons;
