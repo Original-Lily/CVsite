de3b659d43d4927762684c7bba2b3414ea1da6ed
Lily <87139613+Original-Lily@users.noreply.github.com>
2023-11-22 06:37:13 +0000

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,8 +6,7 @@ const data = [
     link: 'https://github.com/Original-Lily/Site',
     image: '/images/projects/CloakRoom.png',
     date: '2023-01-12',
-    desc:
-      'An easy to use and professional website in order to demonstrate my skills & attributes, tasks still todo being: include a german/ english toggle, & to create a dark/ light mode toggle'
+    desc: 'An easy to use and professional website in order to demonstrate my skills & attributes, tasks still todo being: include a german/ english toggle, & to create a dark/ light mode toggle',
   },
   {
     title: 'Lilys Diskwatch',
@@ -15,8 +14,7 @@ const data = [
     link: 'https://github.com/Original-Lily/DiskSan-and-FileIntegrity-Utils',
     image: '/images/projects/DiskWatch.png',
     date: '2021-06-12',
-    desc:
-      'Lightweight Disk utilities, Calculates the SHA512 hash for each file in the "Files" directory. Creates or overwrites a baseline file ("baseline.txt") with file paths and corresponding hash values.'
+    desc: 'Lightweight Disk utilities, Calculates the SHA512 hash for each file in the "Files" directory. Creates or overwrites a baseline file ("baseline.txt") with file paths and corresponding hash values.',
   },
   {
     title: 'Unamed Game',
@@ -24,8 +22,7 @@ const data = [
     link: 'https://github.com/Original-Lily/CuteGame',
     image: '/images/projects/NonameGame.png',
     date: '2015-10-11',
-    desc:
-      'A simple yet addictive python game, developed at first for fun, then adapted into a much smaller version in order to evade my schools internal security systems'
+    desc: 'A simple yet addictive python game, developed at first for fun, then adapted into a much smaller version in order to evade my schools internal security systems',
   },
 ];
 
