ca632e25e353b244799905b687525442e13232f6
Lily <buddie9173@gmail.com>
2024-01-03 02:02:38 +0000

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,31 +1,31 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Azure HoneyPot',
-    subtitle: 'Cloud based HoneyPot made with Azure',
-    link: 'https://github.com/Original-Lily/DiskSan-and-FileIntegrity-Utils',
-    image: '/images/projects/HoneyPot.png',
-    date: '2024-12-29',
-    desc: 'Cloud based HoneyPot made with Microsoft Azure and Sentinel to map login attempts using a geolocation API & Powershell scripts',
-  },
-  {
-    title: 'Nautical Almanac App',
+    title: '🛠️ Nautical Almanac App',
     subtitle: 'Nautical Almanac App for Sailors',
     link: 'https://github.com/CSherv272/NauticalAlmanacApp',
     image: '/images/projects/NautAlmac.png',
     date: '2024-01-02',
-    desc: 'Nautical Almanac App for Sailors in collaboration with a friend, Callum using React Native, NodeJS & Python',
+    desc: 'Nautical Almanac App for Sailors to calculate compass error -  in collaboration with a friend, Callum using React Native, NodeJS & Python',
   },
   {
-    title: 'PhishGuard',
+    title: '✅ Azure HoneyPot',
+    subtitle: 'Cloud based HoneyPot made with Azure',
+    link: 'https://github.com/Original-Lily/DiskSan-and-FileIntegrity-Utils',
+    image: '/images/projects/HoneyPot.png',
+    date: '2023-12-29',
+    desc: 'Cloud based HoneyPot made with Microsoft Azure and Sentinel to map login attempts using a geolocation API & Powershell scripts',
+  },
+  {
+    title: '🛠️ PhishGuard',
     subtitle: 'Phishing Email Detection Tool',
     link: 'https://github.com/Original-Lily/PhishGuard',
     image: '/images/projects/Phish.png',
     date: '2023-12-11',
-    desc: 'An excel and python based tool to detect phishing emails, using a combination of machine learning and natural language processing',
+    desc: 'An excel and python based tool to detect phishing emails, using a combination of machine learning and natural language processing through SKlearn & other modules',
   },
   {
-    title: 'CloakRoom',
+    title: '✅ CloakRoom',
     subtitle: 'CLI messaging app',
     link: 'https://github.com/Original-Lily/CloakRoom',
     image: '/images/projects/CloakRoom.png',
@@ -33,7 +33,7 @@ const data = [
     desc: 'A symmetric or asymmetric encrypted CLI messaging app, made with python',
   },
   {
-    title: 'DeutschW-rter',
+    title: '🛠️ DeutschW-rter',
     subtitle: 'Small german words project',
     link: 'https://github.com/Original-Lily/DeutschW-rter',
     image: '/images/projects/GermProj.png',
@@ -41,7 +41,7 @@ const data = [
     desc: 'A straightforward combination of C++ and Python, recieving words from an Amazon relational database, to pop up on your screen for you to translate.',
   },
   {
-    title: 'Resume Site',
+    title: '✅ Resume Site',
     subtitle: 'Site all about me I can show friends & the workplace',
     link: 'https://github.com/Original-Lily/CVsite',
     image: '/images/projects/Resume.png',
@@ -49,7 +49,7 @@ const data = [
     desc: 'An easy-to-use and professional website in order to demonstrate my skills & attributes, tasks still todo being: include a German/English toggle, & to create a dark/light mode toggle',
   },
   {
-    title: 'Lilys Diskwatch',
+    title: '✅ Lilys Diskwatch',
     subtitle: 'Lightweight Utilities',
     link: 'https://github.com/Original-Lily/DiskSan-and-FileIntegrity-Utils',
     image: '/images/projects/DiskWatch.png',
@@ -57,7 +57,7 @@ const data = [
     desc: 'Lightweight Disk utilities, Calculates the SHA512 hash for each file in the "Files" directory. Creates or overwrites a baseline file ("baseline.txt") with file paths and corresponding hash values.',
   },
   {
-    title: 'Unnamed Game',
+    title: '✅ Unnamed Game',
     subtitle: 'Created at 14',
     link: 'https://github.com/Original-Lily/CuteGame',
     image: '/images/projects/NonameGame.png',
