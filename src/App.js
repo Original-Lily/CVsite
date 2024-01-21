b3324c8dd6d3e9c7d5e9be42cc50105f1951209b
Lily <buddie9173@gmail.com>
2024-01-21 17:35:00 +0000

    Revert "Cursor Prototype"
    This reverts commit b05b534a48d6ae24741fdf19de06e092c7b5e98e.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './layouts/Main'; // fallback for lazy pages
 import './static/css/main.scss'; // All of our styles
-import Cursor from './components/Cursor';
 
 const { PUBLIC_URL } = process.env;
 
@@ -19,9 +18,6 @@ const Stats = lazy(() => import('./pages/Stats'));
 
 const App = () => (
   <BrowserRouter basename={PUBLIC_URL}>
-    <div className="app">
-      <Cursor />
-    </div>
     <Suspense fallback={<Main />}>
       <Routes>
         <Route path="/" element={<Index />} />
