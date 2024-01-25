f46ba7eddd6e07fc967a5f5adacb3bca0a70d204
Lily <buddie9173@gmail.com>
2024-01-25 19:25:42 +0000

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -17,7 +17,7 @@ const Projects = () => (
           <h2><Link to="/projects">Projects</Link></h2>
           <p>A selection of projects that I&apos;m not too ashamed of</p>
           <ul>
-            <li>🌐🌍🌌💻 - Live Service</li>
+            <li>🌍 - Live Service</li>
             <li>✅ - Completed Project</li>
             <li>🛠️ - Under Construction</li>
           </ul>
