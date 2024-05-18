5b719fb1e81e0588eb74e51ea9c4263f649fb481
Lily <buddie9173@gmail.com>
2024-05-18 19:03:09 +0100

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -17,7 +17,7 @@ const Projects = () => (
           <h2><Link to="/projects">Projects</Link></h2>
           <p>A selection of projects that I&apos;m not too ashamed of</p>
           <p>These projects are both academic and extra curricular</p>
-          <p>From live service to work in progress, I endevour to expand my skills where possible!</p>
+          <p>From live service to work in progress I endevour to expand my skills where possible</p>
         </div>
       </header>
       {data.map((project) => (
