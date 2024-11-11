7233f842d07127c810d2d56aa45e89419f93009a
Lily <buddie9173@gmail.com>
2024-11-11 04:08:35 +0000

    MS Update

--- src/components/Contact/ContactIcons.js
+++ src/components/Contact/ContactIcons.js
@@ -10,14 +10,6 @@ import {
 const ContactIcons = () => (
   <div className="icons">
     <a
-      href="https://www.instagram.com/_original_lily"
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label="Instagram"
-    >
-      <FaInstagram />
-    </a>
-    <a
       href="https://github.com/Original-Lily"
       target="_blank"
       rel="noopener noreferrer"
@@ -25,7 +17,7 @@ const ContactIcons = () => (
     >
       <FaGithub />
     </a>
-    <a href="mailto:buddie9173@gmail.com" aria-label="Email">
+    <a href="mailto:Contact@FabulousLily.com" aria-label="Email">
       <FaEnvelope />
     </a>
     <a
